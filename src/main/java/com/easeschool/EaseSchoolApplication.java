8bd9d16fcbe52fa27d75110ebc53273b47c8f8dc
advait pawar <pawaradvait2003@gmail.com>
2024-10-01 17:44:36 +0530

    version 1.0 -home page

---
+++ src/main/java/com/easeschool/EaseSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.easeschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EaseSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EaseSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/easeschool/EaseSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.easeschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EaseSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
