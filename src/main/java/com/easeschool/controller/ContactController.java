9d2d085ff282d9bcfb96b92307e5c2620b4860e4
advait pawar <pawaradvait2003@gmail.com>
2024-10-01 18:27:51 +0530

    Version 1.0 -coontact page

--- src/main/java/com/easeschool/controller/ContactController.java
+++ src/main/java/com/easeschool/controller/ContactController.java
@@ -1,13 +1,30 @@
 package com.easeschool.controller;
 
+import com.easeschool.model.Contact;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ContactController {
 
     @RequestMapping("/contact")
-    public String displayContact(){
+    public String displayContact(Model model) {
+        model.addAttribute("contact", new Contact());
         return "contact.html";
+    }
+
+    @PostMapping("/saveMsg")
+    public String saveMsg(@Valid @ModelAttribute(name = "contact") Contact contact, Errors errors) {
+        if (errors.hasErrors()) {
+            System.out.println(errors.getAllErrors());
+            return "contact.html";
+        }
+ System.out.println(contact);
+        return "redirect:contact";
     }
 }
